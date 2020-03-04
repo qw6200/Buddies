dc9371b2eb512054ee26842b2ee96ea45798529f
Jacob Lee <wlee33@u.rochester.edu>
2020-03-04 18:24:45 -0500

    created map, collisions, and layers working

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -8,23 +8,32 @@ public class PlayerMovement : MonoBehaviour
 
   private Rigidbody2D myRigidbody;
   private Vector3 change;
+  private Animator animator;
 
   // Start is called before the first frame update
   void Start()
   {
-      myRigidbody = GetComponent<Rigidbody2D>();
+    myRigidbody = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
   }
 
   // Update is called once per frame
   void Update()
   {
-      change = Vector3.zero;
-      change.x = Input.GetAxisRaw("Horizontal");
-      change.y = Input.GetAxisRaw("Vertical");
-
-      if (change != Vector3.zero) {
-        MoveCharacter();
-      }
+    change = Vector3.zero;
+    change.x = Input.GetAxisRaw("Horizontal");
+    change.y = Input.GetAxisRaw("Vertical");
+    UpdateAnimationAndMove();
+  }
+  void UpdateAnimationAndMove() {
+    if (change != Vector3.zero) {
+      MoveCharacter();
+      animator.SetFloat("moveX", change.x);
+      animator.SetFloat("moveY", change.y);
+      animator.SetBool("moving", true);
+    } else {
+      animator.SetBool("moving", false);
+    }
   }
 
   void MoveCharacter() {
