98e6ba1cca317621827efbb24d3d6f0eba8e55c0
Jacob Lee <wlee33@u.rochester.edu>
2020-03-05 21:28:37 -0500

    enemy moves directionally

--- Scripts/Enemies/Log/Log.cs
+++ Scripts/Enemies/Log/Log.cs
@@ -30,13 +30,35 @@ public class Log : Enemy
         if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius) {
             if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger) {
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                changeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);    
+                anim.SetBool("wakeUp", true);
             }
-
+        } else if (Vector3.Distance(target.position, transform.position) > chaseRadius){
+            anim.SetBool("wakeUp", false);
         }
     }
-
+    void SetAnimFloat(Vector2 setVector) {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+    void changeAnim(Vector2 direction) {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
+            if (direction.x > 0) {
+                SetAnimFloat(Vector2.right);
+            } else if (direction.x < 0) {
+                SetAnimFloat(Vector2.left);
+            }
+        } else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
+            if (direction.y > 0) {
+                SetAnimFloat(Vector2.up);
+            } else if (direction.y < 0) {
+                SetAnimFloat(Vector2.down);
+            }
+        }
+    }
+    
     void ChangeState(EnemyState newState) {
         if (currentState != newState) {
             currentState = newState;
