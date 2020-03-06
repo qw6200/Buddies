5194419055c11a13be5425cd6f69a5daaff3db8d
Jacob Lee <wlee33@u.rochester.edu>
2020-03-05 20:47:10 -0500

    knockback

--- Scripts/Effects/Knockback.cs
+++ Scripts/Effects/Knockback.cs
@@ -8,23 +8,24 @@ public class Knockback : MonoBehaviour
     public float knockTime;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Enemy")) {
-            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
-            if (enemy != null) {
-                enemy.isKinematic = false;
-                Vector2 difference = enemy.transform.position - transform.position;
-                difference = difference.normalized * thrust;
-                enemy.AddForce(difference, ForceMode2D.Impulse);
-                StartCoroutine(KnockCo(enemy));
-            }
+        if (other.gameObject.CompareTag("breakable") && this.gameObject.CompareTag("Player")) {
+            other.GetComponent<Pot>().Smash();
         }
-    }
-    
-    private IEnumerator KnockCo(Rigidbody2D enemy) {
-        if (enemy != null) {
-            yield return new WaitForSeconds(knockTime);
-            enemy.velocity = Vector2.zero;
-            enemy.isKinematic = true;
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Player")) {
+            Rigidbody2D hit = other.GetComponent<Rigidbody2D>();
+            if (hit != null) {
+                Vector2 difference = hit.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                hit.AddForce(difference, ForceMode2D.Impulse);
+                if (other.gameObject.CompareTag("Enemy")) {
+                    hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
+                    other.GetComponent<Enemy>().Knock(hit, knockTime);
+                }
+                if (other.gameObject.CompareTag("Player")) {
+                    hit.GetComponent<PlayerMovement>().currentState = PlayerState.stagger;
+                    other.GetComponent<PlayerMovement>().Knock(knockTime);
+                }
+            }
         }
     }
 }
--- Scripts/Enemies/Log/Log.cs
+++ Scripts/Enemies/Log/Log.cs
@@ -8,22 +8,38 @@ public class Log : Enemy
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    public Animator anim;
 
+    private Rigidbody2D myRigidbody;
     // Start is called before the first frame update
     void Start()
     {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;    
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         CheckDistance();
     }
 
     void CheckDistance() {
         if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius) {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger) {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);    
+            }
+
+        }
+    }
+
+    void ChangeState(EnemyState newState) {
+        if (currentState != newState) {
+            currentState = newState;
         }
     }
 }
